60fde36b6a482aeb0a24bb8f66dececacc62fea2
Nithya Shikarpur <nithya@Nithyas-MacBook-Air.local>
2022-11-06 23:16:33 -0500

    fixed pitch detection

--- static/js/script.js
+++ static/js/script.js
@@ -1,305 +1,304 @@
-// import Sequencer from './sequencer'; 
-var vizPlayer = new mm.Player();
-
+// Things left to do:
+// 1. Create a separate listening page
+// 2. Create the option to display lyrics on the side
+// 3. Create more pitch contours for the songs
+// 4. Implement other functionality - record, slow down, transpose etc.
 
 //globals
 let envelope;
 let buttons = new Map();
 let notes = new Map();
 let current_tab;
-// let notes = undefined;
 let current_col;
 let prev;
 let score = 0;
 let current_note;
 let detected;
 let runPitch = false;
-
-// function practice1_notes(){
-//     return {notes: [
-//             { pitch: 60, startTime: 0.0, endTime: 2.0 },
-//             { pitch: 62, startTime: 2.0, endTime: 4.0 },
-//             { pitch: 64, startTime: 4.0, endTime: 6.0 },
-//             { pitch: 62, startTime: 6.0, endTime: 8.0 },
-//             { pitch: 60, startTime: 8.0, endTime: 10.0 },
-//             { pitch: 62, startTime: 10.0, endTime: 12.0 },
-//             { pitch: 64, startTime: 12.0, endTime: 14.0 },
-//         ],
-//         tempos: [{
-//             time: 0, 
-//             qpm: 120
-//             }],
-//             totalTime: 14.0
-//         };
-// }
+let currentPitch = 0;
+let currentTime = 0;
+let startTime = 0;
+let audioContext;
+var buflen = 2048;
+var buf = new Float32Array( buflen );
+var pitchTimer = null;
+var drawTimer = null;
 
 
-// function practice2_notes(){
-//     return {notes: [
-//             { pitch: 60, startTime: 0.0, endTime: 3.0 },
-//             { pitch: 62, startTime: 3.0, endTime: 6.0 },
-//             { pitch: 64, startTime: 6.0, endTime: 9.0 },
-//             { pitch: 60, startTime: 9.0, endTime: 12.0 },
-//             { pitch: 62, startTime: 12.0, endTime: 15.0 },
-//             { pitch: 64, startTime: 15.0, endTime: 18.0 },
-//         ],
-//         tempos: [{
-//             time: 0, 
-//             qpm: 120
-//             }],
-//             totalTime: 18.0
-//         };
-// }
-
-// function practice3_notes(){
-//     return {notes: [
-//             { pitch: 60, startTime: 0.0, endTime: 3.0 },
-//             { pitch: 62, startTime: 3.0, endTime: 6.0 },
-//             { pitch: 64, startTime: 6.0, endTime: 9.0 },
-//             { pitch: 62, startTime: 9.0, endTime: 12.0 },
-//             { pitch: 60, startTime: 12.0, endTime: 15.0 },
-//             { pitch: 62, startTime: 15.0, endTime: 18.0 },
-//             { pitch: 64, startTime: 18.0, endTime: 21.0 },
-//             { pitch: 62, startTime: 21.0, endTime: 24.0 },
-//             { pitch: 60, startTime: 24.0, endTime: 27.0 },
-//         ],
-//         tempos: [{
-//             time: 0, 
-//             qpm: 120
-//             }],
-//             totalTime: 27.0
-//         };
-// }
-
-// async function generateNotes(sequencer) {
-//     await rnnLoaded;
-//     let seed = {
-//         notes: [
-//             { pitch: 60, startTime: 0.0, endTime: 4.0 },
-//             { pitch: 62, startTime: 4.0, endTime: 8.0 },
-//             { pitch: 64, startTime: 8.0, endTime: 12.0 },
-//             { pitch: 62, startTime: 12.0, endTime: 16.0 },
-//             { pitch: 60, startTime: 16.0, endTime: 20.0 },
-//             { pitch: 62, startTime: 4.0, endTime: 8.0 },
-//             { pitch: 64, startTime: 8.0, endTime: 12.0 },
-//             { pitch: 62, startTime: 12.0, endTime: 16.0 },
-//             { pitch: 60, startTime: 16.0, endTime: 20.0 },
-//         ],
-//         tempos: [{
-//         time: 0, 
-//         qpm: 120
-//         }],
-//         totalTime: 3.0
-//     };
-    
-//     var rnn_steps = 124; // (time span detection: rnn_steps-10)
-//     var rnn_temp = 0;
-//     var chord_prog = ['C'];
-//     const qns = mm.sequences.quantizeNoteSequence(seed, 1);
-
-//     var notes = await melodyRnn.continueSequence(qns, rnn_steps, rnn_temp, chord_prog);
-//     return notes
-// };
-
-document.getElementById("practice-test").onclick = () => {;
-    // current_tab = "test1";
+document.getElementById("practice-test").onclick = () => {
     startPractice()
     console.log('starting practice')
 }
 
-// document.getElementById("practice-2").onclick = () => {
-//     current_tab = "test4";
-//     startPractice("test4")
-// }
+document.getElementById("stop").onclick = () => {
+    stopPractice();
+    console.log('stopping practice');
+}
 
-// document.getElementById("practice-3").onclick = () => {
-//     current_tab = "test5";
-//     startPractice("test5")
-// }
+function stopPractice() {
+    runPitch = false;
+}
 
 function startPractice() {
-    // sequencer = sequencers.get(sequencer_id);
-    // envelope = new Envelope('envelope');
-    // sequencerStop();
-    // sequencer.resetSequencer();
-    // sequencer.resetCounter(); 
     runPitch = true;
     setup();
-    // vizPlayer = new mm.Player(false, {
-    // run: (note) => {
-    //     current_col += 1;
-    //     sequencer.next();
-    //     detected = false;
-    //     current_note = note;
-    // },
-    // stop: () => {
-    //     sequencerStop();
-    // }
-    // });
-
-    // vizPlayer.start(notes.get(current_tab));
 };
 
 // Pitch Detection
-let audioContext;
+
 let mic;
 let pitch;
 let stream;
 
 async function setup() {
-    const audioContext = new AudioContext();
+    audioContext = new AudioContext();
     console.log('sample rate', audioContext.sampleRate)
     stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-    console.log(audioContext, stream);
     
-    //console.log(orig_x, orig_y);
+    // initialise plots
     await Plotly.newPlot('myDiv', 
     [{
+        
         x:Array.from(Array(10).keys()),
         y:Array(10).fill(null)
+       
     },
-{
+    {
+    mode: 'lines',
+        x:Array.from(Array(10).keys()),
+        y:Array(10).fill(null),
+        text: Array(10).fill('lala'),
+        textposition: top
+    },
+    {
+    mode: 'text',
     x:Array.from(Array(10).keys()),
-    y:Array(10).fill(null)
-}], 
-{
-    yaxis: {
-        range: [-500, 1900]
+    y:Array(10).fill(null),
+        text: Array(10).fill('lala')
     },
-    xaxis: {
-        range: [0, 10]
-    }
-});
+    ], 
+    {
+        yaxis: {
+            range: [-500, 1900]
+        },
+        xaxis: {
+            range: [-3, 7]
+        },
+        shapes: [
+
+        //line vertical
+
+        {
+        type: 'line',
+        x0: 0,
+        y0: -500,
+        x1: 0,
+        y1: 1900,
+        line: {
+            color: 'rgb(55, 128, 191)',
+            width: 3
+        }
+        }]
+    });
    
-    startPitch(stream, audioContext);
+    startPitch(stream);
+    requestAnimationFrame(draw);
 }
 
-function startPitch(stream, audioContext) {
-   
-    Plotly.d3.csv("static/pitch/Rolling In The Deep.csv", function(data){ processData(data) } );
-    pitch = ml5.pitchDetection('static/js/model', audioContext , stream, modelLoaded); // not sure what the sample rate is
+function startPitch(stream) {
+    // read data from csv and plot the OG contour
+    console.log('in startPitch');
+    Plotly.d3.csv("static/pitch/RITD.csv", function(data){ processData(data) } );
+    startTime = new Date();
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    setInterval(shiftPlot, 500);    // call to shift the plot every 500 ms
+    pitch = ml5.pitchDetection('static/js/model', audioContext , stream, modelLoaded); // pitch detection
+    
 }
+
+// function autoCorrelate( buf, sampleRate ) {
+// 	// Implements the ACF2+ algorithm
+// 	var SIZE = buf.length;
+//     console.log('size', SIZE);
+// 	var rms = 0;
+
+// 	for (var i=0;i<SIZE;i++) {
+// 		var val = buf[i];
+// 		rms += val*val;
+// 	}
+// 	rms = Math.sqrt(rms/SIZE);
+// 	if (rms<0.01) // not enough signal
+// 		return -1;
+
+// 	var r1=0, r2=SIZE-1, thres=0.2;
+// 	for (var i=0; i<SIZE/2; i++)
+// 		if (Math.abs(buf[i])<thres) { r1=i; break; }
+// 	for (var i=1; i<SIZE/2; i++)
+// 		if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
+
+// 	buf = buf.slice(r1,r2);
+// 	SIZE = buf.length;
+
+// 	var c = new Array(SIZE).fill(0);
+// 	for (var i=0; i<SIZE; i++)
+// 		for (var j=0; j<SIZE-i; j++)
+// 			c[i] = c[i] + buf[j]*buf[j+i];
+
+// 	var d=0; while (c[d]>c[d+1]) d++;
+// 	var maxval=-1, maxpos=-1;
+// 	for (var i=d; i<SIZE; i++) {
+// 		if (c[i] > maxval) {
+// 			maxval = c[i];
+// 			maxpos = i;
+// 		}
+// 	}
+//     // console.log('maxpos', maxpos);
+// 	var T0 = maxpos;
+
+// 	var x1=c[T0-1], x2=c[T0], x3=c[T0+1];
+// 	a = (x1 + x3 - 2*x2)/2;
+// 	b = (x3 - x1)/2;
+// 	if (a) T0 = T0 - b/(2*a);
+//     // console.log('T0', T0);
+// 	return sampleRate/T0;
+// }
+
+// function updatePitch() {
+//     console.log('in updatePitch', audioContext, currentPitch, currentTime);
+//     if (runPitch) {
+//         tonic = 261.63;
+//         pitchLims = [196, 783.99]
+//         analyser.getFloatTimeDomainData( buf );
+//         var ac = autoCorrelate( buf, audioContext.sampleRate );
+//         // var ac = 0
+//         // TODO: Paint confidence meter on canvasElem here.
+
+//         if (ac == -1) {
+//             currentPitch = null;
+//             currentTime = (new Date() - startTime)/1000;
+//         } else {
+//     currentPitch = 1200*Math.log2(ac/tonic);
+    
+//     //if ((currentPitch < pitchLims[0]) | (currentPitch > pitchLims[1]))   currentPitch = null;
+//     currentTime = (new Date() - startTime)/1000;
+//         }
+//         console.log('ac', ac, 'currentPitch', currentPitch, 'currentTime', currentTime);
+    
+// 	//rafID = window.requestAnimationFrame( updatePitch );
+//     if (runPitch) setTimeout(function(){
+//         updatePitch();
+//     }, 10);
+// }
+// else{
+//     clearInterval(pitchTimer);
+//     // clearInterval(drawTimer);
+// }
+// }
 
 function modelLoaded() {
-    ind = 300
     console.log('in callback')
-    getPitch(ind);
+    getPitch();
 }
    
 function processData(allRows) {
 
    console.log(allRows);
-   var x = [], y = [];
+   var x = [], y = [], words = [], ytext = [], xtext = [];
+   prev_y = 500;
 
    for (var i=0; i<allRows.length; i++) {
        row = allRows[i];
-       x.push( parseFloat(row['time']) + 3);
+       x.push( parseFloat(row['time']));
        y.push( row['pitch'] );
+       console.log(i, row)
+        console.log('logging word: ', row['word'])
+        if (row['pitch'] == ''){
+        if(row['word'] != ''){
+            xtext.push(row['time'])
+            ytext.push(prev_y);
+            words.push(row['word']);
+        }
+    }
+    else {
+        prev_y = row['pitch'];
+        if(row['word'] != '') {
+        xtext.push(row['time'])
+        ytext.push(prev_y);
+        words.push(row['word']);
+    }
+    }
+
    }
-   console.log( 'X',x, 'Y',y );
+   console.log( 'X',x, 'Y',y, 'words', words );
    Plotly.extendTraces('myDiv', {
-       x: [x], // the model detects pitches at every 0.01s by default
-     y: [y]
+    // mode: 'lines+text',
+    x: [x], // the model detects pitches at every 0.01s by default
+    y: [y],
    }, [1]);
+   Plotly.extendTraces('myDiv', {
+    // mode: 'lines+text',
+    x: [xtext], // the model detects pitches at every 0.01s by default
+    y: [ytext],
+    text: [words]
+   }, [2]);
 }
 
-function getPitch(ind) {
-    pitch.getPitch(function(err, frequency) {
-        tonic = 261.63
-        console.log(err, frequency)
-        ind++;
-        if (frequency) {
-            Plotly.extendTraces('myDiv', {
-                x: [[(ind*0.01)]], // the model detects pitches at every 0.01s by default
-              y: [[1200*Math.log2(frequency/tonic)*(100/100)]]
+function draw() {
+    
+    Plotly.extendTraces('myDiv', {
+                x: [[currentTime]], // the model detects pitches at every 0.01s by default
+              y: [[currentPitch]]
             }, [0]);
+    if (runPitch) {
+        request = requestAnimationFrame(draw);
+    }
+}
+
+function shiftPlot() {
+    // shifts plot
+    if(runPitch) {
+        var secs = currentTime;
+        Plotly.update('myDiv', {},{
+            shapes: [
+
+            //line vertical
+
+            {
+              type: 'line',
+              x0: secs,
+              y0: -500,
+              x1: secs,
+              y1: 1900,
+              line: {
+                color: 'rgba(55, 128, 191, 0.5)',
+                width: 3
+              }
+            }, []]
+        })
+        Plotly.relayout('myDiv', {
+            xaxis: {
+                range: [secs-3, secs + 7]
+            }
+        });
+    }
+}
+
+function getPitch() {
+    // gets pitch
+    pitch.getPitch(function(err, frequency) {
+        tonic = 261.63; // hardcoded tonic for now, needs to be read from file
+        console.log(err, frequency, currentTime);
+        if (frequency) {
+            currentPitch = 1200*Math.log2(frequency/tonic);
+            currentTime = (new Date() - startTime)/1000;
         }
         else{
-            Plotly.extendTraces('myDiv', {
-                x: [[(ind*0.01)]],
-              y: [[null]]
-            }, [0]);
+            currentPitch = null;
+            currentTime = (new Date() - startTime)/1000;
         }
-        if (ind % 100) {
-            console.log('ind update', ind)
-            Plotly.relayout('myDiv', {
-                xaxis: {
-                    range: [(ind/100)-3, (ind/100)+7]
-                }
-            });
-        }
-        if(runPitch) getPitch(ind);
+        if(runPitch) getPitch();
     })
 }
 
 
-document.getElementById("stop").onclick = async () => {
-    sequencerStop();
-    vizPlayer.stop();
-};
-
-document.getElementById("stop-2").onclick = async () => {
-    sequencerStop();
-    vizPlayer.stop();
-};
-
-document.getElementById("stop-3").onclick = async () => {
-    sequencerStop();
-    vizPlayer.stop();
-};
-
-function toggleButton(id){
-    var x = document.getElementById(id);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-function sequencerStop(){
-    localStorage.setItem(score_label(), score);
-    current_col = 0;
-    prev = null;
-    runPitch = false;
-    score = 0;
-    toggleButton(buttons.get(current_tab)["stop"]);
-    toggleButton(buttons.get(current_tab)["practice"]);
-}
-
-function score_label(){
-    return `#${current_tab}-score`
-}
-
-// Lesson Sections
-var el = document.querySelector('.tabs');
-var instance = M.Tabs.init(el, {});
-
-document.getElementById("tabs-button-1").onclick = () => { 
-    document.getElementById('tab2').classList.remove("disabled")
-    var el = document.querySelector('.tabs');
-    var instance = M.Tabs.init(el, {});
-    instance.select('tab2');
-    instance.updateTabIndicator();
-}
-document.getElementById("tabs-button-2").onclick = () => { 
-    document.getElementById('tab3').classList.remove("disabled")
-    var el = document.querySelector('.tabs');
-    var instance = M.Tabs.init(el, {});
-    instance.select('tab3');
-    instance.updateTabIndicator();
-}
-document.getElementById("tabs-button-3").onclick = () => { 
-    document.getElementById('tab4').classList.remove("disabled")
-    var el = document.querySelector('.tabs');
-    var instance = M.Tabs.init(el, {});
-    instance.select('tab4');
-    instance.updateTabIndicator();
-}
-document.getElementById("tabs-button-4").onclick = () => { 
-    document.getElementById('tab5').classList.remove("disabled")
-    var el = document.querySelector('.tabs');
-    var instance = M.Tabs.init(el, {});
-    instance.select('tab5');
-    instance.updateTabIndicator();
-}
