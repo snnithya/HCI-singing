e7bb608f2674134f7a6abfdd635276541b137b8c
Nithya Shikarpur <nithya@Nithyas-MacBook-Air.local>
2022-11-17 15:24:58 -0500

    added a hacky score functionality

--- static/js/report.js
+++ static/js/report.js
@@ -15,7 +15,6 @@ let score = 0;
 let current_note;
 let detected;
 let runPitch = false;
-let currentPitch = 0;
 let currentTime = 0;
 let startTime = 0;
 let audioContext;
@@ -25,6 +24,12 @@ var buf = new Float32Array( buflen );
 var pitchTimer = null;
 var drawTimer = null;
 var path = "static/pitch/RITD-100-0.csv";
+var x = [];
+var y = [];
+var plot_ind = 1;
+var counter = 3;
+var incorrect = 0;
+var correct = 0;
 
 window.onload = function() {
     setup();
@@ -40,10 +45,23 @@ async function setup() {
     
     // initialise plots
     await Plotly.newPlot('myDiv', 
-    [{
-        
+    [{ // correct plots
+        type: 'scatter',
+        mode: 'lines',
         x:Array.from(Array(10).keys()),
-        y:Array(10).fill(null)
+        y:Array(10).fill(null),
+        line: {
+            color: 'rgb(0, 128, 0)'
+        }
+
+    },
+        { // incorrect plots
+        mode: 'lines',
+        x:Array.from(Array(10).keys()),
+        y:Array(10).fill(null),
+        line: {
+            color: 'rgb(128, 0, 0)'
+        }
        
     },
     {
@@ -51,7 +69,10 @@ async function setup() {
         x:Array.from(Array(10).keys()),
         y:Array(10).fill(null),
         text: Array(10).fill('lala'),
-        textposition: top
+        textposition: top,
+        line: {
+            color: 'rgb(128, 128, 128)'
+        }
     },
     {
     mode: 'text',
@@ -63,9 +84,6 @@ async function setup() {
     {
         yaxis: {
             range: [-500, 1900]
-        },
-        xaxis: {
-            range: [-3, 7]
         }
         // shapes: [
 
@@ -98,25 +116,66 @@ function plotPitch() {
     // plot singer's pitch
     fetch("/getmethod")
     .then((response) => response.json())
-    .then((data) => Plotly.extendTraces('myDiv', {
-        x: [data["x"]], 
-      y: [data["y"]]
-    }, [0]));
+    .then((data) => { 
+        Plotly.extendTraces('myDiv', {
+                x: [data["x"]], 
+                y: [data["y"]]
+        }, [0]);
+        for (var i = 0; i < data["x"].length; i++) { 
+            // console.log(i)
+            time_ind = x.findIndex(element => {
+            if (element >= data["x"][i]) {
+                return true;
+              }
+              return false;
+            })
+            orig_y = y[time_ind]
+            // if (typeof orig_y === 'string' && orig_y.length !== 0 && !!data["y"][i]) {
+            console.log(time_ind, orig_y, data["y"][i], (Math.abs(orig_y - data["y"][i]) <= 100))
+            // if (counter >0) {
+            //     // counter = 3;
+            if ((Math.abs(orig_y - data["y"][i]) <= 200)) {
+                // plot_ind = 1
+                // Plotly.extendTraces('myDiv', {
+                //         x: [[data["x"][i]]], 
+                //         y: [[data["y"][i]]]
+                // }, [1]);
+                Plotly.extendTraces('myDiv', {
+                        x: [[data["x"][i]]], 
+                        y: [[null]]
+                }, [1]);
+                correct++;
+            }
+            else {
+                incorrect++;
+                Plotly.extendTraces('myDiv', {
+                        x: [[data["x"][i]]], 
+                        y: [[data["y"][i]]]
+                }, [1]);
+                
+            }}
+            // counter--;
+            // console.log(counter, plot_ind)
+            
+        // }
+        document.getElementById('score').innerHTML = 'Score: ' + String(Math.round(((correct)/(correct + incorrect))*10)) + '/10'
+            // console.log(plot_ind, orig_y, data["y"][i], x[time_ind], data["x"][i])
+});
 
-    
 }
-
 function processData(allRows) {
 
-   console.log(allRows);
-   var x = [], y = [], words = [], ytext = [], xtext = [];
+//    console.log(allRows);
+   x = [], y = []
+   var words = [], ytext = [], xtext = [];
    prev_y = 500;
 
    for (var i=0; i<allRows.length; i++) {
        row = allRows[i];
        x.push( parseFloat(row['time']));
        y.push( row['pitch'] );
-       console.log(i, row)
+       console.log(row['pitch'], (row['pitch'] !== null))
+    //    console.log(i, row)
         console.log('logging word: ', row['word'])
         if (row['pitch'] == ''){
         if(row['word'] != ''){
@@ -140,13 +199,13 @@ function processData(allRows) {
     // mode: 'lines+text',
     x: [x], // the model detects pitches at every 0.01s by default
     y: [y],
-   }, [1]);
+   }, [2]);
    Plotly.extendTraces('myDiv', {
     // mode: 'lines+text',
     x: [xtext], // the model detects pitches at every 0.01s by default
     y: [ytext],
     text: [words]
-   }, [2]);
+   }, [3]);
 }
 
 
--- static/js/singing.js
+++ static/js/singing.js
@@ -18,19 +18,27 @@ let runPitch = false;
 let currentPitch = 0;
 let currentTime = 0;
 let startTime = 0;
-let audioContext;
 var recorder;
 var buflen = 2048;
 var buf = new Float32Array( buflen );
 var pitchTimer = null;
 var drawTimer = null;
 var path = "static/pitch/RITD-100-0.csv";
+var x = [];
+var y = [];
+var pitch;
+var mic;
+let audioContext;
 
-document.getElementById("practice-test").onclick = () => {
-    startPractice()
-    console.log('starting practice')
+window.onload = function() {
+    setup;
 }
 
+// document.getElementById("practice-test").onclick = () => {
+//     startPractice()
+//     console.log('starting practice')
+// }
+document.getElementById("practice-test").addEventListener("click", micListen);
 document.getElementById("stop").onclick = () => {
     stopPractice();
     // console.log('stopping practice');
@@ -48,38 +56,43 @@ function stopPractice() {
     })
 }
 
+
 function startPractice() {
-    runPitch = true;
+    // runPitch = true;
     setup();
 };
 
 // Pitch Detection
 
-let mic;
-let pitch;
-let stream;
-
 async function setup() {
-    audioContext = new AudioContext();
-    console.log('sample rate', audioContext.sampleRate)
-    stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-    
     // initialise plots
     await Plotly.newPlot('myDiv', 
-    [{
+    [{// correctly sung parts
         
         x:Array.from(Array(10).keys()),
-        y:Array(10).fill(null)
+        y:Array(10).fill(null),
        
     },
-    {
+    // {// incorrectly sung parts
+
+    //     x:Array.from(Array(10).keys()),
+    //     y:Array(10).fill(null),
+    //     line: {
+    //         color: 'rgb(128, 0, 0)'
+    //     }
+
+    // },
+    {// contour of original song
     mode: 'lines',
         x:Array.from(Array(10).keys()),
         y:Array(10).fill(null),
         text: Array(10).fill('lala'),
-        textposition: top
+        textposition: top,
+        line: {
+            color: 'rgb(128, 128, 128)'
+        }
     },
-    {
+    {// words from the song
     mode: 'text',
     x:Array.from(Array(10).keys()),
     y:Array(10).fill(null),
@@ -109,112 +122,37 @@ async function setup() {
         }
         }]
     });
-
-    // let recordStream = await navigator.mediaDevices.getUserMedia({canvas: true, audio: true});
-    // recorder = new RecordRTCPromisesHandler(document.getElementById('myDiv'), {
-    //     type: 'audio'
-    // });
-   
-    startPitch(stream);
-    requestAnimationFrame(draw);
+    plotOrigPitch(path);
+    audioContext = getAudioContext();
+    mic = await new p5.AudioIn();
+    // mic.start(startPitch);
 }
 
 function plotOrigPitch(path) {
-    console.log('in plot orgi pitch', path);
+    // console.log('in plot orgi pitch', path);
     Plotly.d3.csv(path, function(data){ processData(data) } );
 }
 
-function startPitch(stream) {
-    // read data from csv and plot the OG contour
-    console.log('in startPitch');
-    // recorder.startRecording();
-    plotOrigPitch(path);
+function micListen() {
+    mic.start(startPitch);
+    runPitch = true;
     startTime = new Date();
-    // mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-    setInterval(shiftPlot, 250);    // call to shift the plot every 500 ms
-    pitch = ml5.pitchDetection('static/js/model', audioContext , stream, modelLoaded); // pitch detection
-    
 }
 
-// function autoCorrelate( buf, sampleRate ) {
-// 	// Implements the ACF2+ algorithm
-// 	var SIZE = buf.length;
-//     console.log('size', SIZE);
-// 	var rms = 0;
+async function startPitch() {
+    console.log('in startPitch');
 
-// 	for (var i=0;i<SIZE;i++) {
-// 		var val = buf[i];
-// 		rms += val*val;
-// 	}
-// 	rms = Math.sqrt(rms/SIZE);
-// 	if (rms<0.01) // not enough signal
-// 		return -1;
-
-// 	var r1=0, r2=SIZE-1, thres=0.2;
-// 	for (var i=0; i<SIZE/2; i++)
-// 		if (Math.abs(buf[i])<thres) { r1=i; break; }
-// 	for (var i=1; i<SIZE/2; i++)
-// 		if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
-
-// 	buf = buf.slice(r1,r2);
-// 	SIZE = buf.length;
-
-// 	var c = new Array(SIZE).fill(0);
-// 	for (var i=0; i<SIZE; i++)
-// 		for (var j=0; j<SIZE-i; j++)
-// 			c[i] = c[i] + buf[j]*buf[j+i];
-
-// 	var d=0; while (c[d]>c[d+1]) d++;
-// 	var maxval=-1, maxpos=-1;
-// 	for (var i=d; i<SIZE; i++) {
-// 		if (c[i] > maxval) {
-// 			maxval = c[i];
-// 			maxpos = i;
-// 		}
-// 	}
-//     // console.log('maxpos', maxpos);
-// 	var T0 = maxpos;
-
-// 	var x1=c[T0-1], x2=c[T0], x3=c[T0+1];
-// 	a = (x1 + x3 - 2*x2)/2;
-// 	b = (x3 - x1)/2;
-// 	if (a) T0 = T0 - b/(2*a);
-//     // console.log('T0', T0);
-// 	return sampleRate/T0;
-// }
-
-// function updatePitch() {
-//     console.log('in updatePitch', audioContext, currentPitch, currentTime);
-//     if (runPitch) {
-//         tonic = 261.63;
-//         pitchLims = [196, 783.99]
-//         analyser.getFloatTimeDomainData( buf );
-//         var ac = autoCorrelate( buf, audioContext.sampleRate );
-//         // var ac = 0
-//         // TODO: Paint confidence meter on canvasElem here.
-
-//         if (ac == -1) {
-//             currentPitch = null;
-//             currentTime = (new Date() - startTime)/1000;
-//         } else {
-//     currentPitch = 1200*Math.log2(ac/tonic);
+    // let audioContext = new AudioContext();
+    // console.log('sample rate', audioContext.sampleRate)
+    // let stream = navigator.mediaDevices.getUserMedia({ audio: true, video: false });
     
-//     //if ((currentPitch < pitchLims[0]) | (currentPitch > pitchLims[1]))   currentPitch = null;
-//     currentTime = (new Date() - startTime)/1000;
-//         }
-//         console.log('ac', ac, 'currentPitch', currentPitch, 'currentTime', currentTime);
-    
-// 	//rafID = window.requestAnimationFrame( updatePitch );
-//     if (runPitch) setTimeout(function(){
-//         updatePitch();
-//     }, 10);
-// }
-// else{
-//     clearInterval(pitchTimer);
-//     // clearInterval(drawTimer);
-// }
-// }
+    // mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    setInterval(shiftPlot, 5);    // call to shift the plot every 500 ms
+    // console.log(audioContext, stream);
+    pitch = ml5.pitchDetection('static/js/model', audioContext , mic.stream, modelLoaded); // pitch detection
+    requestAnimationFrame(true_draw);
+}
 
 function modelLoaded() {
     console.log('in callback')
@@ -223,16 +161,16 @@ function modelLoaded() {
    
 function processData(allRows) {
 
-   console.log(allRows);
-   var x = [], y = [], words = [], ytext = [], xtext = [];
+//    console.log(allRows);
+   var words = [], ytext = [], xtext = [];
    prev_y = 500;
 
    for (var i=0; i<allRows.length; i++) {
        row = allRows[i];
        x.push( parseFloat(row['time']));
        y.push( row['pitch'] );
-       console.log(i, row)
-        console.log('logging word: ', row['word'])
+    //    console.log(i, row)
+        // console.log('logging word: ', row['word'])
         if (row['pitch'] == ''){
         if(row['word'] != ''){
             xtext.push(row['time'])
@@ -250,10 +188,9 @@ function processData(allRows) {
     }
 
    }
-   console.log( 'X',x, 'Y',y, 'words', words );
+//    console.log( 'X',x, 'Y',y, 'words', words );
    Plotly.extendTraces('myDiv', {
-    // mode: 'lines+text',
-    x: [x], // the model detects pitches at every 0.01s by default
+    x: [x], 
     y: [y],
    }, [1]);
    Plotly.extendTraces('myDiv', {
@@ -264,14 +201,30 @@ function processData(allRows) {
    }, [2]);
 }
 
-function draw() {
-    
+function true_draw() {
+    // console.log('in draw', runPitch)
+    // time_ind = x.findIndex(element => {
+    //   if (element >= currentTime) {
+    //     return true;
+    //   }
+
+    //   return false;
+    // })
+    // orig_y = y[time_ind]
+    // console.log(time_ind, orig_y, currentPitch, (currentPitch >= orig_y - 30 && currentPitch <= orig_y + 30))
+    // if (currentPitch >= orig_y - 30 && currentPitch <= orig_y + 30) {
+    //     plot_ind = 0 
+    // }
+    // else {
+    //     plot_ind = 1
+    // }
+    // console.log(plot_ind, orig_y, time_ind)
     Plotly.extendTraces('myDiv', {
                 x: [[currentTime]], // the model detects pitches at every 0.01s by default
-              y: [[currentPitch]]
+              y: [[currentPitch]],
             }, [0]);
     if (runPitch) {
-        request = requestAnimationFrame(draw);
+        request = requestAnimationFrame(true_draw);
     }
 }
 
@@ -306,6 +259,7 @@ function shiftPlot() {
 
 function getPitch() {
     // gets pitch
+    console.log('in getPitch', runPitch, pitch)
     pitch.getPitch(function(err, frequency) {
         tonic = 261.63; // hardcoded tonic for now, needs to be read from file
         console.log(err, frequency, currentTime);
