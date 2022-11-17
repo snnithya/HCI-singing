2e92ad907ecdbf73c4b6dfecb2f92fe8de3951d7
lukewys <wuyusongwys@gmail.com>
2022-11-17 18:15:15 -0500

    update relative path

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 # API
 @app.route('/')
 def index():
-    return render_template('library.html')
-
+    return render_template('index.html')
 
 if __name__ == "__main__": 
    app.run(debug=True)
